6da5b32ed754c35e81d35b97d1bef04e34de334c
din0x <superrobertxd52@gmail.com>
2023-06-15 19:58:58 +0200

    Update Program.cs

--- Quark/Program.cs
+++ Quark/Program.cs
@@ -4,13 +4,23 @@ namespace QuarkLang;
 
 internal class Program
 {
-    private static void Main(string[] args)
-    {
-#if DEBUG
-        Console.WriteLine("Mode: DEBUG");
-#endif
-        if (args.Length <= 0) return;
+	private static void Main(string[] args)
+	{
+		if (args.Length <= 0) 
+			return;
 
-        Core.Quark.Run(args[0]);
-    }
+		if (args[0] == "help" && args[0] == "-h")
+			DisplayHelpMessage();
+
+		Core.Quark.Run(args[0]);
+	}
+
+	private static void DisplayHelpMessage()
+	{
+		var help = """
+		
+		""";
+
+		Console.WriteLine(help);
+	}
 }
